4e1ef211f43897653cc92f925d1bdb186de31517
Vitor Gomes <vitorgomes_silva@outlook.com>
2024-03-16 18:09:12 -0300

    Embed all three buttons into a VStack. Create a variable 'screenSize' to capture screen size bounds. Add 'login with email' button with a .frame modifier set to 90% of the screen width, fixed height of 48, and a RoundedRectangle shape with .clipShape modifier. Set 'login with Microsoft' button with a .frame modifier of 90% screen width and 48 fixed height. Decrease font size of SSO button. Decrease font size of 'Olá' Text view. Embed version and privacy terms Text views into a VStack and add a .frame modifier to this VStack set to 90% of the screen width

--- DynaPredictReplica/Views/LoginView.swift
+++ DynaPredictReplica/Views/LoginView.swift
@@ -9,40 +9,51 @@ import SwiftUI
 
 struct LoginView: View {
     
+    private var screenSize = UIScreen.main.bounds
     
     var body: some View {
         Text("Olá!")
-            .font(.system(size: 32))
+            .font(.system(size: 24))
             .bold()
             .foregroundStyle(.defaultBlue)
         
-        Button {
+        VStack {
+            Button {
+                
+            } label: {
+                Text("ENTRAR COM E-MAIL E SENHA")
+                    .foregroundStyle(.white)
+            }
+            .frame(width: screenSize.width * 0.9, height: 48)
+            .background(.defaultBlue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
             
-        } label: {
-            Text("ENTRAR COM E-MAIL E SENHA")
-                .foregroundStyle(.white)
-        }
-        .background(.defaultBlue)
-        
-        Button {
+            Button {
+                
+            } label: {
+                Text("ENTRAR COM MICROSOFT")
+                    .foregroundStyle(.defaultBlue)
+            }
+            .frame(width: screenSize.width * 0.9, height: 48)
             
-        } label: {
-            Text("ENTRAR COM MICROSOFT")
-                .foregroundStyle(.defaultBlue)
+            Button {
+                
+            } label: {
+                Text("Continuar com SSO (Single Sign On)")
+                    .font(.system(size: 14))
+                    .foregroundStyle(.defaultBlue)
+            }
         }
         
-        Button {
+        VStack {
+            Text("Versão 1.0")
+                .foregroundStyle(.gray)
             
-        } label: {
-            Text("Continuar com SSO (Single Sign On)")
-                .foregroundStyle(.defaultBlue)
+            Text("Ao avançar, declaro que li e concordo com os Termos Gerais e Condições de Uso e o Aviso de Privacidade.")
+                .foregroundStyle(.gray)
         }
+        .frame(width: screenSize.width * 0.9)
         
-        Text("Versão 1.0")
-            .foregroundStyle(.gray)
-        
-        Text("Ao avançar, declaro que li e concordo com os Termos Gerais e Condições de Uso e o Aviso de Privacidade.")
-            .foregroundStyle(.gray)
     }
 }
 
