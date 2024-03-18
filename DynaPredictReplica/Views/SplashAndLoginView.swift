a5623753f96f53c8e6c78b7ba3b6f6212ed59cdd
Vitor Gomes <vitorgomes_silva@outlook.com>
2024-03-17 23:01:43 -0300

    Start animation of dynaPredictCube on SplashAndLoginView. Create a State variable 'shouldAnimate' and add .onAppear() modifier with withAnimation() using an easeInOut animation to set shouldAnimate to true. Apply shouldAnimate to .scaleEffect() of dynaPredictCube

--- DynaPredictReplica/Views/SplashAndLoginView.swift
+++ DynaPredictReplica/Views/SplashAndLoginView.swift
@@ -10,18 +10,24 @@ import SwiftUI
 struct SplashAndLoginView: View {
     private var screenSize = UIScreen.main.bounds
     
+    @State private var shouldAnimate: Bool = false
+    
     var body: some View {
         ZStack {
-            Image("splashBackground")
-                // Since the image was a screenshot from the original app i had to stretch it to take all the bounds
+            Image("splashBackground") // Since the image was a screenshot from the original app i had to stretch it to take all the bounds
                 .resizable()
                 .scaledToFill()
                 .frame(width: screenSize.width * 1, height: screenSize.height * 1)
                 .ignoresSafeArea()
             
             Image("dynaPredictCube")
-                .resizable()
-                .scaleEffect(x: 0.23, y: 0.1)
+                //.resizable()
+                .scaleEffect(shouldAnimate ? 0.3 : 0.5)
+                .onAppear {
+                    withAnimation(Animation.easeInOut(duration: 1.0).repeatCount(2, autoreverses: true)) {
+                        self.shouldAnimate = true
+                    }
+                }
             
             Text("DynaPredict")
                 .font(.system(size: 24))
