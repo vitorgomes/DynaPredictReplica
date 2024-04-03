01434b286c3331ca6d3bf37397ee2cce2167f842
Vitor Gomes <vitorgomes_silva@outlook.com>
2024-04-02 21:03:16 -0300

    Enhance toolbar functionality by adding a NavigationStack as the main view, applying defaultBlue color with .toolbarBackground modifier to ensure consistent appearance. Introduce feature to display toolbar color even when static. Include two SVG images ('menu' and 'filter') and create corresponding toolbarItems. Implement a HStack below the NavigationStack with a new color from Assets (toolBarBlue) and a text view to display the current filter name, adjusting sizes using .frame modifier

--- DynaPredictReplica/Views/MachinesListView.swift
+++ DynaPredictReplica/Views/MachinesListView.swift
@@ -11,60 +11,80 @@ struct MachinesListView: View {
     private let screenSize = UIScreen.main.bounds
     
     var body: some View {
-        ZStack(alignment: .bottomTrailing) {
-            ScrollView {
-                HStack {
-                    Button {
-                        // TODO: Add some action
-                    } label: {
-                        Image(systemName: "camera.circle.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .foregroundColor(.defaultBlue)
-                    }
-                    .frame(width: screenSize.width * 0.2, height: screenSize.height * 0.1)
-                    
-                    VStack(alignment: .leading) {
-                        Text("Name")
-                            .bold()
+        NavigationStack {
+            HStack(alignment: .center) {
+                Text("All")
+                    .foregroundStyle(.white)
+            }
+            .frame(width: screenSize.width * 1, height: 32)
+            .background(Color.toolBarBlue)
+            
+            ZStack(alignment: .bottomTrailing) {
+                ScrollView {
+                    HStack {
+                        Button {
+                            // TODO: Add some action
+                        } label: {
+                            Image(systemName: "camera.circle.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundColor(.defaultBlue)
+                        }
+                        .frame(width: screenSize.width * 0.2, height: screenSize.height * 0.1)
                         
-                        Text("Principal - Syncronyzed")
-                            .foregroundStyle(.gray)
+                        VStack(alignment: .leading) {
+                            Text("Name")
+                                .bold()
+                            
+                            Text("Principal - Syncronyzed")
+                                .foregroundStyle(.gray)
+                        }
+                        .padding(.leading, 16)
+                        
+                        VStack(spacing: 3) {
+                            Text("31 Apr")
+                            
+                            Image(systemName: "checkmark.circle.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: screenSize.width * 0.08, height: screenSize.height * 0.04)
+                                .foregroundColor(.green)
+                        }
+                        .padding([.top, .bottom], 24)
+                        
                     }
+                    .frame(width: screenSize.width * 0.9, height: screenSize.height * 0.1)
                     .padding(.leading, 16)
                     
-                    VStack(spacing: 3) {
-                        Text("31 Apr")
-                        
-                        Image(systemName: "checkmark.circle.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: screenSize.width * 0.08, height: screenSize.height * 0.04)
-                            .foregroundColor(.green)
-                    }
-                    .padding([.top, .bottom], 24)
-                    
+                    Divider()
                 }
-                .frame(width: screenSize.width * 0.9, height: screenSize.height * 0.1)
-                .padding(.leading, 16)
                 
-                Divider()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.cyan)
+                        .frame(width: screenSize.width * 0.22, height: screenSize.height * 0.22)
+                        .padding(.trailing, 16)
+                }
             }
-            
-            Button {
-                
-            } label: {
-                Image(systemName: "plus.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundColor(.cyan)
-                    .frame(width: screenSize.width * 0.22, height: screenSize.height * 0.22)
-                    .padding(.trailing, 16)
+        }
+        .toolbarBackground(Color.defaultBlue, for: .navigationBar).toolbarBackground(.visible, for: .navigationBar)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Image("menu")
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Image("filter")
             }
         }
     }
 }
 
 #Preview {
-    MachinesListView()
+    NavigationStack {
+        MachinesListView()
+    }
 }
