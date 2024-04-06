d322851e500c3f4035e234e17a036c20fb46a1eb
Vitor Gomes <vitorgomes_silva@outlook.com>
2024-04-05 22:31:28 -0300

    Introduce Models folder. Implement LoginModelValidator class for validation logic and define a User struct with email and login properties to represent user data

---
+++ DynaPredictReplica/Models/LoginModelValidator.swift
@@ -0,0 +1,11 @@
+//
+//  LoginModelValidator.swift
+//  DynaPredictReplica
+//
+//  Created by Vitor Gomes on 05/04/24.
+//
+
+import Foundation
+
+class LoginModelValidator {
+}
---
+++ DynaPredictReplica/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  DynaPredictReplica
+//
+//  Created by Vitor Gomes on 05/04/24.
+//
+
+import Foundation
+
+struct User {
+    let email: String
+    let password: String
+}
